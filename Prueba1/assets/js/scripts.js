ae3490a7099d0dd172cf1f10d8ca98791074e01f
mjfv <85653371+mjfv@users.noreply.github.com>
2021-06-09 21:40:08 -0400

    Archivos subidos manualmente

---
+++ Prueba1/assets/js/scripts.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault(); // Prevent default anchor click behavior
+            
+            var hash = this.hash; // Store hash
+              	$('html, body').animate({
+                	scrollTop: $(hash).offset().top
+                }, 800, function(){ // milliseconds it takes to scroll to the specified area
+                	window.location.hash = hash;
+              	});
+        }
+    });
+
+	//Transparent and color navbar
+	
+      window.addEventListener('scroll', function () {
+        if (window.pageYOffset > 100) {  //scroll
+          $('nav').addClass('bg-dark');
+        } else { 
+          $('nav').removeClass('bg-dark');
+        }
+      }); 
+
+	//Navbar active -- Bootstrap
+	var scrollSpy = new bootstrap.ScrollSpy(document.body, {
+		target: '#navbarNav'
+	});
+
+	//Tooltip -- Bootstrap
+	var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+	var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+  		return new bootstrap.Tooltip(tooltipTriggerEl)
+	});
+
+	//Carousel -- Bootstrap
+	var myCarousel = document.querySelector('#myCarousel');
+	var carousel = new bootstrap.Carousel(myCarousel, {
+		interval: 2000,
+		wrap: true,
+		pause: 'hover'
+	});
+	
+
+
+});
